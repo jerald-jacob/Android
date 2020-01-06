dd363e454b7db4da9f9885d4eeb02fab8b03ef69
jerald-jacob <jeraldjacob10@gmail.com>
2020-01-06 17:44:57 +0530

    GET posts from url manupulation implemented

--- GetApi/app/src/main/java/com/camrininfotech/getapi/JsonPlaceHolderApi.java
+++ GetApi/app/src/main/java/com/camrininfotech/getapi/JsonPlaceHolderApi.java
@@ -1,12 +1,28 @@
 package com.camrininfotech.getapi;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("userId")Integer[] userId,
+            @Query("userId")int userId2,
+            @Query("_sort")String sort,
+            @Query("_order")String order);
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String ,String>parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id")int posId);
+    @GET
+    Call<List<Comment>>getComments(@Url String url);
 }
--- GetApi/app/src/main/java/com/camrininfotech/getapi/MainActivity.java
+++ GetApi/app/src/main/java/com/camrininfotech/getapi/MainActivity.java
@@ -6,7 +6,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +32,19 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        getPosts();
+        //getComments();
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+    }
+
+    private void getPosts() {
+        Map<String,String>parameters=new HashMap<>();
+        parameters.put("userId","1");
+        parameters.put("_sort","id");
+        parameters.put("order","desc");
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
+        //new Integer[]{2,3,4},2,"id","desc"
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -57,6 +70,38 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments("posts/3/comments");
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Comment>comments=response.body();
+              //  assert comments != null;
+                for(Comment comment:comments){
+
+                    String content = "";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n\n";
+
+                    textViewResult.append(content);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
